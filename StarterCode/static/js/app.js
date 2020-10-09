24210c4c203b25294f817d451f56c4391d92b9a6
hauckc21 <hauck.chris@gmail.com>
2020-10-08 21:05:30 -0500

    data loaded

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,11 +1,37 @@
 console.log("This is app.js");
 
-function DrawBubblechart(sampleId) {
-    console.log(`DrawBarGraph(${sampleId})`);
-}
-
-function optionChanged(newSampleId) {
-    console.log(`User selected ${newSampleId}`);
-}
-
 function InitDashboard()
+{
+    console.log(`Calling InitDashboard()`);
+
+    var selector = d3.select("#selDataset");
+
+    //load the data
+    d3.json("samples.json").then((data) => {
+        console.log(data);
+
+        //Get the names
+        var sampleNames = data.names;
+        });
+}
+
+InitDashboard();
+
+
+
+
+// function DrawBubblechart(sampleId) {
+//     console.log(`DrawBarGraph(${sampleId})`);
+// }
+
+// function optionChanged(newSampleId) {
+//     console.log(`User selected ${newSampleId}`);
+// }
+
+
+
+// //populate the selector with all the sample Ids
+// sampleNames.forEach((sampleId) => {
+//     selector.append("option")
+//         .text(sampleId)
+//         .property("value", sampleID);
