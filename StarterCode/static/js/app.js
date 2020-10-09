946928c7d3b290dc8b1eaf583582e5efdd2fbdad
hauckc21 <hauck.chris@gmail.com>
2020-10-08 20:58:45 -0500

    id function

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,11 @@
+console.log("This is app.js");
+
+function DrawBubblechart(sampleId) {
+    console.log(`DrawBarGraph(${sampleId})`);
+}
+
+function optionChanged(newSampleId) {
+    console.log(`User selected ${newSampleId}`);
+}
+
+function InitDashboard()
